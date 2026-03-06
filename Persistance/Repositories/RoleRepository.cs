4913029c55dd93d48986816979a06a0810c775fa
Bahadır Can <bahadir.can@ideateknoloji.com.tr>
2026-03-06 21:30:29 +0300

    Test amaçlı role interface, role class ve roleController oluşturuldu.

---
+++ Persistance/Repositories/RoleRepository.cs
@@ -0,0 +1,31 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RoleRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Role role)
+        {
+            await _context.Roles.AddAsync(role); // Eğer AppDbContext'te DbSet adın Roles değilse burayı düzelt
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
